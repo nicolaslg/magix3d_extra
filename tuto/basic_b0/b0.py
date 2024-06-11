7108ee15666a9fc36d22ec6c252186036fc4dec2
nicolas le goff <nicolas.le-goff@cea.fr>
2024-06-12 00:31:02 +0200

    updated basic_bo tuto

--- tuto/basic_b0/b0.py
+++ tuto/basic_b0/b0.py
@@ -11,6 +11,12 @@ ctx.setLengthUnit(Mgx3D.Unit.centimeter)
 ctx.getGeomManager().importSTEP("B0.step")
 # Création d'un bloc topologique structuré sans projection (Vol0000)
 ctx.getTopoManager().newFreeTopoOnGeometry ("Vol0000")
+# Changement de discrétisation pour Ar0000
+emp = Mgx3D.EdgeMeshingPropertyUniform(30)
+ctx.getTopoManager().setParallelMeshingProperty (emp,"Ar0000")
+# Changement de discrétisation pour Ar0011
+emp = Mgx3D.EdgeMeshingPropertyUniform(20)
+ctx.getTopoManager().setParallelMeshingProperty (emp,"Ar0011")
 # Affectation d'une projection vers Pt0008 pour les entités topologiques Som0005
 ctx.getTopoManager().setGeomAssociation (["Som0005"], "Pt0008", True)
 # Affectation d'une projection vers Pt0009 pour les entités topologiques Som0007
@@ -27,8 +33,6 @@ ctx.getTopoManager().setGeomAssociation (["Som0002"], "Pt0002", True)
 ctx.getTopoManager().setGeomAssociation (["Som0004"], "Pt0000", True)
 # Affectation d'une projection vers Pt0001 pour les entités topologiques Som0006
 ctx.getTopoManager().setGeomAssociation (["Som0006"], "Pt0001", True)
-# Suppression des projections pour les entités topologiques Ar0009
-ctx.getTopoManager().setGeomAssociation (["Ar0009"], "", True)
 # Affectation d'une projection vers Crb0007 pour les entités topologiques Ar0009
 ctx.getTopoManager().setGeomAssociation (["Ar0009"], "Crb0007", True)
 # Affectation d'une projection vers Crb0013 pour les entités topologiques Ar0011
@@ -45,6 +49,10 @@ ctx.getTopoManager().setGeomAssociation (["Ar0008"], "Crb0003", True)
 ctx.getTopoManager().setGeomAssociation (["Ar0010"], "Crb0001", True)
 # Affectation d'une projection vers Crb0000 pour les entités topologiques Ar0006
 ctx.getTopoManager().setGeomAssociation (["Ar0006"], "Crb0000", True)
+# Affectation d'une projection vers Crb0020 pour les entités topologiques Ar0005
+ctx.getTopoManager().setGeomAssociation (["Ar0005"], "Crb0020", True)
+# Affectation d'une projection vers Crb0002 pour les entités topologiques Ar0004
+ctx.getTopoManager().setGeomAssociation (["Ar0004"], "Crb0002", True)
 # Affectation d'une projection vers Surf0002 pour les entités topologiques Fa0005
 ctx.getTopoManager().setGeomAssociation (["Fa0005"], "Surf0002", True)
 # Affectation d'une projection vers Surf0008 pour les entités topologiques Fa0001
@@ -55,57 +63,63 @@ ctx.getTopoManager().setGeomAssociation (["Fa0000"], "Surf0000", True)
 ctx.getTopoManager().setGeomAssociation (["Fa0002"], "Surf0001", True)
 # Affectation d'une projection vers Surf0003 pour les entités topologiques Fa0003
 ctx.getTopoManager().setGeomAssociation (["Fa0003"], "Surf0003", True)
-# Découpage en O-grid des blocs structurés Bl0000
-ctx.getTopoManager().splitBlocksWithOgridV2 (["Bl0000"], ["Fa0002", "Fa0004", "Fa0003"], .5, 10)
-# Destruction des entités topologiques Bl0004
-ctx.getTopoManager().destroy (["Bl0004"], True)
-# Découpage du bloc Bl0003
-ctx.getTopoManager().splitBlock ("Bl0003","Ar0002",.5)
-# Affectation d'une projection vers Pt0006 pour les entités topologiques Som0009
-ctx.getTopoManager().setGeomAssociation (["Som0009"], "Pt0006", True)
-# Affectation d'une projection vers Pt0012 pour les entités topologiques Som0011
-ctx.getTopoManager().setGeomAssociation (["Som0011"], "Pt0012", True)
-# Affectation d'une projection vers Pt0004 pour les entités topologiques Som0008
-ctx.getTopoManager().setGeomAssociation (["Som0008"], "Pt0004", True)
-# Affectation d'une projection vers Pt0010 pour les entités topologiques Som0010
-ctx.getTopoManager().setGeomAssociation (["Som0010"], "Pt0010", True)
-# Affectation d'une projection vers Pt0005 pour les entités topologiques Som0018
-ctx.getTopoManager().setGeomAssociation (["Som0018"], "Pt0005", True)
-# Affectation d'une projection vers Pt0013 pour les entités topologiques Som0019
-ctx.getTopoManager().setGeomAssociation (["Som0019"], "Pt0013", True)
-# Affectation d'une projection vers Crb0005 pour les entités topologiques Som0013
-ctx.getTopoManager().setGeomAssociation (["Som0013"], "Crb0005", True)
-# Affectation d'une projection vers Crb0015 pour les entités topologiques Som0015
-ctx.getTopoManager().setGeomAssociation (["Som0015"], "Crb0015", True)
-# Suppression des projections pour les entités topologiques Som0015
-ctx.getTopoManager().setGeomAssociation (["Som0015"], "", True)
-# Affectation d'une projection vers Crb0016 pour les entités topologiques Som0014
-ctx.getTopoManager().setGeomAssociation (["Som0014"], "Crb0016", True)
-# Affectation d'une projection vers Crb0004 pour les entités topologiques Som0012
-ctx.getTopoManager().setGeomAssociation (["Som0012"], "Crb0004", True)
-# Affectation d'une projection vers Crb0004 pour les entités topologiques Ar0021 Ar0037
-ctx.getTopoManager().setGeomAssociation (["Ar0021", "Ar0037"], "Crb0004", True)
-# Affectation d'une projection vers Crb0005 pour les entités topologiques Ar0038 Ar0025
-ctx.getTopoManager().setGeomAssociation (["Ar0038", "Ar0025"], "Crb0005", True)
-# Affectation d'une projection vers Crb0016 pour les entités topologiques Ar0023 Ar0040
-ctx.getTopoManager().setGeomAssociation (["Ar0023", "Ar0040"], "Crb0016", True)
-# Affectation d'une projection vers Crb0015 pour les entités topologiques Ar0027 Ar0041
-ctx.getTopoManager().setGeomAssociation (["Ar0027", "Ar0041"], "Crb0015", True)
-# Affectation d'une projection vers Crb0019 pour les entités topologiques Ar0024
-ctx.getTopoManager().setGeomAssociation (["Ar0024"], "Crb0019", True)
-# Affectation d'une projection vers Crb0017 pour les entités topologiques Ar0020
-ctx.getTopoManager().setGeomAssociation (["Ar0020"], "Crb0017", True)
-# Affectation d'une projection vers Surf0006 pour les entités topologiques Ar0026
-ctx.getTopoManager().setGeomAssociation (["Ar0026"], "Surf0006", True)
-# Affectation d'une projection vers Surf0005 pour les entités topologiques Ar0022
-ctx.getTopoManager().setGeomAssociation (["Ar0022"], "Surf0005", True)
-# Affectation d'une projection vers Crb0018 pour les entités topologiques Ar0043
-ctx.getTopoManager().setGeomAssociation (["Ar0043"], "Crb0018", True)
-# Affectation d'une projection vers Surf0007 pour les entités topologiques Fa0010
-ctx.getTopoManager().setGeomAssociation (["Fa0010"], "Surf0007", True)
-# Affectation d'une projection vers Surf0004 pour les entités topologiques Fa0006
-ctx.getTopoManager().setGeomAssociation (["Fa0006"], "Surf0004", True)
-# Affectation d'une projection vers Surf0005 pour les entités topologiques Fa0016 Fa0028
-ctx.getTopoManager().setGeomAssociation (["Fa0016", "Fa0028"], "Surf0005", True)
-# Affectation d'une projection vers Surf0006 pour les entités topologiques Fa0017 Fa0029
-ctx.getTopoManager().setGeomAssociation (["Fa0017", "Fa0029"], "Surf0006", True)
+# Découpage du bloc Bl0000
+ctx.getTopoManager().splitBlock ("Bl0000","Ar0002",.9)
+# Découpage du bloc Bl0001
+ctx.getTopoManager().splitBlock ("Bl0001","Ar0012",.1)
+# Découpage de tous les blocs suivant l'arête Ar0009
+ctx.getTopoManager().splitAllBlocks ("Ar0009",.8)
+# Découpage en O-grid des blocs structurés Bl0008
+ctx.getTopoManager().splitBlocksWithOgridV2 (["Bl0008"], ["Fa0034", "Fa0020", "Fa0035"], .5, 10)
+# Destruction des entités topologiques Bl0014
+ctx.getTopoManager().destroy (["Bl0014"], True)
+# Découpage de tous les blocs suivant l'arête Ar0025
+ctx.getTopoManager().splitAllBlocks ("Ar0025",.5)
+# Affectation d'une projection vers Pt0006 pour les entités topologiques Som0024
+ctx.getTopoManager().setGeomAssociation (["Som0024"], "Pt0006", True)
+# Affectation d'une projection vers Pt0012 pour les entités topologiques Som0025
+ctx.getTopoManager().setGeomAssociation (["Som0025"], "Pt0012", True)
+# Affectation d'une projection vers Pt0010 pour les entités topologiques Som0027
+ctx.getTopoManager().setGeomAssociation (["Som0027"], "Pt0010", True)
+# Affectation d'une projection vers Pt0004 pour les entités topologiques Som0026
+ctx.getTopoManager().setGeomAssociation (["Som0026"], "Pt0004", True)
+# Affectation d'une projection vers Pt0005 pour les entités topologiques Som0037
+ctx.getTopoManager().setGeomAssociation (["Som0037"], "Pt0005", True)
+# Affectation d'une projection vers Pt0013 pour les entités topologiques Som0036
+ctx.getTopoManager().setGeomAssociation (["Som0036"], "Pt0013", True)
+# Affectation d'une projection vers Crb0005 pour les entités topologiques Som0028
+ctx.getTopoManager().setGeomAssociation (["Som0028"], "Crb0005", True)
+# Affectation d'une projection vers Crb0004 pour les entités topologiques Som0030
+ctx.getTopoManager().setGeomAssociation (["Som0030"], "Crb0004", True)
+# Affectation d'une projection vers Crb0016 pour les entités topologiques Som0031
+ctx.getTopoManager().setGeomAssociation (["Som0031"], "Crb0016", True)
+# Affectation d'une projection vers Crb0015 pour les entités topologiques Som0029
+ctx.getTopoManager().setGeomAssociation (["Som0029"], "Crb0015", True)
+# Affectation d'une projection vers Crb0017 pour les entités topologiques Ar0076
+ctx.getTopoManager().setGeomAssociation (["Ar0076"], "Crb0017", True)
+# Affectation d'une projection vers Crb0019 pour les entités topologiques Ar0071
+ctx.getTopoManager().setGeomAssociation (["Ar0071"], "Crb0019", True)
+# Affectation d'une projection vers Crb0004 pour les entités topologiques Ar0075 Ar0098
+ctx.getTopoManager().setGeomAssociation (["Ar0075", "Ar0098"], "Crb0004", True)
+# Affectation d'une projection vers Crb0005 pour les entités topologiques Ar0097 Ar0070
+ctx.getTopoManager().setGeomAssociation (["Ar0097", "Ar0070"], "Crb0005", True)
+# Affectation d'une projection vers Crb0015 pour les entités topologiques Ar0072 Ar0094
+ctx.getTopoManager().setGeomAssociation (["Ar0072", "Ar0094"], "Crb0015", True)
+# Affectation d'une projection vers Crb0016 pour les entités topologiques Ar0095 Ar0077
+ctx.getTopoManager().setGeomAssociation (["Ar0095", "Ar0077"], "Crb0016", True)
+# Affectation d'une projection vers Crb0014 pour les entités topologiques Som0010 Ar0018 Ar0063
+ctx.getTopoManager().setGeomAssociation (["Som0010", "Ar0018", "Ar0063"], "Crb0014", True)
+# Affectation d'une projection vers Crb0012 pour les entités topologiques Som0014 Ar0065 Ar0029
+ctx.getTopoManager().setGeomAssociation (["Som0014", "Ar0065", "Ar0029"], "Crb0012", True)
+# Affectation d'une projection vers Crb0009 pour les entités topologiques Som0013 Ar0026 Ar0064
+ctx.getTopoManager().setGeomAssociation (["Som0013", "Ar0026", "Ar0064"], "Crb0009", True)
+# Affectation d'une projection vers Crb0006 pour les entités topologiques Som0009 Ar0062 Ar0015
+ctx.getTopoManager().setGeomAssociation (["Som0009", "Ar0062", "Ar0015"], "Crb0006", True)
+# Affectation d'une projection vers Surf0004 pour les entités topologiques Ar0034 Fa0019 Fa0053
+ctx.getTopoManager().setGeomAssociation (["Ar0034", "Fa0019", "Fa0053"], "Surf0004", True)
+# Affectation d'une projection vers Surf0007 pour les entités topologiques Ar0022 Fa0048 Fa0011
+ctx.getTopoManager().setGeomAssociation (["Ar0022", "Fa0048", "Fa0011"], "Surf0007", True)
+# Affectation d'une projection vers Surf0005 pour les entités topologiques Ar0074 Fa0058 Fa0076
+ctx.getTopoManager().setGeomAssociation (["Ar0074", "Fa0058", "Fa0076"], "Surf0005", True)
+# Affectation d'une projection vers Surf0006 pour les entités topologiques Ar0073 Fa0077 Fa0057
+ctx.getTopoManager().setGeomAssociation (["Ar0073", "Fa0077", "Fa0057"], "Surf0006", True)
--- tuto/basic_b0/b0_tuto.md
+++ tuto/basic_b0/b0_tuto.md
@@ -8,11 +8,15 @@ This is the geom model
 
 ![2D geom Image](geom.png)
 
-Create a free structured block. An associated block cannot be created because the shape is too complex for magix3d to automatically associate the topo entities to the geometry.
+Create a free structured block associated to the volume (topo->block->create). A not free block cannot be created because the shape is too complex for magix3d to automatically associate the topo entities to the geometry. By choosing the volume the block initial position will be the bounding box of the volume.
 
 ![2D topo Image](block.png)
 
-Associate the entites of the blocking to the geom model. Not all of them can be associated.
+Change the discretization of the edges (default is 10x10x10)
+
+![2D topo Image](discretize.png)
+
+Associate the entites of the blocking to the geom model. Not all of them can be associated. You can associate the entities one by one or use "topo->vert/edge/face->assoc" and choose method "Vertices to geom entities". The same goes for the edges.
 
 ![split ogrid Image](assoc2.png)
 
@@ -20,19 +24,31 @@ Do not forget to also do it for the faces
 
 ![split ogrid Image](assoc.png)
 
-Apply an ogrid pattern to the block. Ensure that it imprints itself onto three of the faces,as shown here in the preview.
+Split the block, here at 0.9
+
+![split ogrid Image](split_r.png)
+
+Proceed one the other side at 0.1
+
+![split ogrid Image](split_l.png)
+
+And at the top, this time split all the blocks at 0.8
+
+![split ogrid Image](split_t.png)
+
+Apply an ogrid pattern to the central block. Ensure that it imprints itself onto three of the faces,as shown here in the preview.
 
 ![split ogrid Image](ogrid_preview.png)
 
-Once applied, this is what the blocking looks like, both faces and edges
+Once applied, this is what the blocking looks like, for both the blocks and then edges
 
-![split ogrid Image](ogrid_faces.png) ![split ogrid Image](ogrid_edges.png)
+![split ogrid Image](ogrid_blocks.png) ![split ogrid Image](ogrid_edges.png)
 
 Destroy the central bottom block.
 
 ![split ogrid Image](destroy.png)
 
-And split the block at the top.
+And split the blocks at the top. It is necessary because at the moment we can assign an edge only to one single geometric entity, and the half circle at the bottom is composed of two curves.
 
 ![split ogrid Image](split.png)
 
